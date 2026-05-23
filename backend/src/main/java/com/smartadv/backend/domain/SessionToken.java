8e615f21c799af36abcaf8b96002e7dbd3134fa1
Yujun LEE <yujunlee0786@ajou.ac.kr>
2026-05-23 16:55:02 +0900

    Fix LazyInitializationException in SessionToken

--- backend/src/main/java/com/smartadv/backend/domain/SessionToken.java
+++ backend/src/main/java/com/smartadv/backend/domain/SessionToken.java
@@ -20,7 +20,7 @@ public class SessionToken {
     @Id
     private String token;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
